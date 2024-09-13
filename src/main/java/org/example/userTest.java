9982a84a4f3125e9bcb147b216212bd1fc0d60cc
0xc0debabe <kongminoo@naver.com>
2024-09-13 13:00:34 +0900

    [2024-09-13] 토비의 스프링 공부
    똑같이 따라쳤는데 Db 연결 부분에서 오래걸렸다.
    Dependency로 추가해서 해결완료

---
+++ src/main/java/org/example/userTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.dao.UserDao;
+import org.example.domain.User;
+
+import java.sql.SQLException;
+
+public class userTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("kong");
+        user.setName("홍민우");
+        user.setPassword("secret");
+
+        dao.add(user);
+        System.out.println(user.getId() + "등록 성공");
+
+        User user1 = dao.get(user.getId());
+        System.out.println(user1.getName());
+        System.out.println(user1.getPassword());
+
+        System.out.println(user1.getId() + "등록 성공");
+    }
+}
