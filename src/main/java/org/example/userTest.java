1507223bf6dd45cede26b3a9c3c7809f41cd07ac
0xc0debabe <kongminoo@naver.com>
2024-09-13 16:52:16 +0900

    [2024-09-13] 토비의 스프링 공부
    인터페이스를 이용한 상속

--- src/main/java/org/example/userTest.java
+++ src/main/java/org/example/userTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.example.company.n;
+import org.example.dao.ConnectionMaker;
+import org.example.dao.DConnectionMaker;
 import org.example.dao.UserDao;
 import org.example.domain.User;
 
@@ -8,7 +10,8 @@ import java.sql.SQLException;
 
 public class userTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new n();
+        ConnectionMaker dConnectionMaker = new DConnectionMaker();
+        UserDao dao = new UserDao(dConnectionMaker);
 
         User user = new User();
 
