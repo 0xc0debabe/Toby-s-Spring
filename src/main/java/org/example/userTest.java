6c3d95adc550d28181ceb28388f183e54f18399d
0xc0debabe <kongminoo@naver.com>
2024-09-15 16:28:06 +0900

    [2024-09-15] 토비의 스프링 공부
    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 내용 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    1. 의존관계
    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-

--- src/main/java/org/example/userTest.java
+++ src/main/java/org/example/userTest.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import org.example.company.n;
-import org.example.dao.ConnectionMaker;
-import org.example.dao.DConnectionMaker;
-import org.example.dao.DaoFactory;
-import org.example.dao.UserDao;
+import org.example.dao.*;
 import org.example.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,20 +11,10 @@ import java.sql.SQLException;
 public class userTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user = new User();
+        UserDao userDao = context.getBean("userDao", UserDao.class);
 
-        user.setId("kong");
-        user.setName("홍민우");
-        user.setPassword("secret");
-
-        dao.add(user);
-        System.out.println(user.getId() + "등록 성공");
-
-        User user1 = dao.get(user.getId());
-        System.out.println(user1.getName());
-        System.out.println(user1.getPassword());
-
-        System.out.println(user1.getId() + "등록 성공");
+        userDao.add(new User());
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println(ccm.getCounter());
     }
 }
